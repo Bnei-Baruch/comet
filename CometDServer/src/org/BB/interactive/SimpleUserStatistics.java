ddc6849736bf95f7451fb2857b3cbc8ff1414e46
Kolman <kolmanv@gmail.com>
2011-09-19 19:03:05 +0300

    For debug.

--- CometDServer/src/org/BB/interactive/SimpleUserStatistics.java
+++ CometDServer/src/org/BB/interactive/SimpleUserStatistics.java
@@ -40,7 +40,7 @@ public class SimpleUserStatistics extends AbstractService {
         		messages.put(message.getChannel(), 1);
         	}
 
-   		    //System.err.println(message.getJSON());
+   		    System.err.println(message.getJSON());
 
         	Object pageObj = message.get("page");
     		if (pageObj instanceof String) {
